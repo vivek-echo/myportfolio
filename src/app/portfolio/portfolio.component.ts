f556cfaf1caf6e5561a9b0d6bda077509d2fa050
vivek jha <vivekjha0151@gmail.com>
2022-08-07 05:14:52 +0530

    changes

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare const $ : any;
 
 @Component({
   selector: 'app-portfolio',
@@ -6,11 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
-  particlesJS: any;
   constructor() { }
 
   ngOnInit(): void {
-    this.particlesJS.load('particles-js', 'particles.json', null);
+    $('#heroSvgImgMob').hide();
+    $('#heroSvgImgWeb').hide();
+    const widthScreen = $(window).width();
+    if(widthScreen < 400){
+      $('#heroSvgImgMob').show();
+      $('.profileSec').css('height','700px');
+      $('.cardbody1').css('border-bottom','1px solid grey')
+      $('.cardbody2').css('border-bottom','1px solid grey')
+
+    }else
+    {
+      $('#heroSvgImgWeb').show();
+      $('.cardbody1').css('border-right','2px solid grey')
+      $('.cardbody2').css('border-right','2px solid grey')
+    }
+
   }
 
 }
