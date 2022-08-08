a75483f0c5f4dcd30ccc3009d272dcf173e95501
vivek jha <vivekjha0151@gmail.com>
2022-08-08 18:19:59 +0530

    modifictaion

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -12,8 +12,39 @@ export class PortfolioComponent implements OnInit {
   ngOnInit(): void {
     $('#heroSvgImgMob').hide();
     $('#heroSvgImgWeb').hide();
+
     const widthScreen = $(window).width();
-    if(widthScreen < 400){
+    $('.imgsection').hover(
+      function () {
+        $('.imgsection').css('transform', 'scale(1.3)');
+        $('.imgsection').css('transition', '.5s');
+        $('#prode').show();
+        $('.imgsection').css('border-radius', '9px');
+        const scrWidth =parseInt( $('.section1').css('width'));
+// alert(scrWidth);
+        if(widthScreen > 800){
+          $('.imgsection').css('width', '38rem');
+          $('#prode').css('top', '10px');
+          $('#prode').css('left', '14rem');
+          $('#prode').css('width', '22rem');
+        }
+        // else{
+        //   $('.imgsection').css('width', '16rem');
+        //   $('.imgsection').css('height', '27rem');
+        //   // $('#prode').css('left', '1rem');
+        // }
+
+      },
+      function () {
+        $('.imgsection').css('transform', 'scale(1)');
+        $('.imgsection').css('width', '13rem');
+        $('.imgsection').css('height', '13rem');
+        $('.imgsection').css('border-radius', '50%');
+        $('.imgsection').css('transition', '.5s');
+        $('#prode').hide();
+      }
+    );
+    if(widthScreen < 900){
       $('#heroSvgImgMob').show();
       $('.profileSec').css('height','700px');
       $('.cardbody1').css('border-bottom','1px solid grey')
@@ -22,8 +53,8 @@ export class PortfolioComponent implements OnInit {
     }else
     {
       $('#heroSvgImgWeb').show();
-      $('.cardbody1').css('border-right','2px solid grey')
-      $('.cardbody2').css('border-right','2px solid grey')
+      $('.cardbody1').css('border-right','1px solid grey')
+      $('.cardbody2').css('border-right','1px solid grey')
     }
 
   }
