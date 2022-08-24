2c271c58f441da88d953ddc0b5114e582f18e938
vivek jha <vivekjha0151@gmail.com>
2022-08-25 00:36:17 +0530

    changes

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-declare const $ : any;
+// import './../../../assets/js/smtp.js';
+ let Email: any;
+// import ‘./../../../assets/js/smtp.js’;
+declare const $: any;
 
 @Component({
   selector: 'app-portfolio',
@@ -10,53 +13,65 @@ export class PortfolioComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $('#desktop').fadeIn(1000)
     $('#heroSvgImgMob').hide();
     $('#heroSvgImgWeb').hide();
 
     const widthScreen = $(window).width();
-    $('.imgsection').hover(
-      function () {
-        $('.imgsection').css('transform', 'scale(1.3)');
-        $('.imgsection').css('transition', '.5s');
-        $('#prode').show();
-        $('.imgsection').css('border-radius', '9px');
-        const scrWidth =parseInt( $('.section1').css('width'));
-// alert(scrWidth);
-        if(widthScreen > 800){
+    if (widthScreen > 800) {
+      $('.imgsection').hover(
+        function () {
+          $('.imgsection').css('transform', 'scale(1.3)');
+          $('.imgsection').css('transition', '.5s');
+          $('#prode').show();
+          $('.imgsection').css('border-radius', '9px');
+
+
           $('.imgsection').css('width', '38rem');
           $('#prode').css('top', '10px');
           $('#prode').css('left', '14rem');
           $('#prode').css('width', '22rem');
+
+          // else{
+          //   $('.imgsection').css('width', '16rem');
+          //   $('.imgsection').css('height', '27rem');
+          //   // $('#prode').css('left', '1rem');
+          // }
+
+        },
+        function () {
+          $('.imgsection').css('transform', 'scale(1)');
+          $('.imgsection').css('width', '13rem');
+          $('.imgsection').css('height', '13rem');
+          $('.imgsection').css('border-radius', '50%');
+          $('.imgsection').css('transition', '.5s');
+          $('#prode').hide();
         }
-        // else{
-        //   $('.imgsection').css('width', '16rem');
-        //   $('.imgsection').css('height', '27rem');
-        //   // $('#prode').css('left', '1rem');
-        // }
+      );
+    }
 
-      },
-      function () {
-        $('.imgsection').css('transform', 'scale(1)');
-        $('.imgsection').css('width', '13rem');
-        $('.imgsection').css('height', '13rem');
-        $('.imgsection').css('border-radius', '50%');
-        $('.imgsection').css('transition', '.5s');
-        $('#prode').hide();
-      }
-    );
-    if(widthScreen < 900){
+    if (widthScreen < 900) {
       $('#heroSvgImgMob').show();
-      $('.profileSec').css('height','700px');
-      $('.cardbody1').css('border-bottom','1px solid grey')
-      $('.cardbody2').css('border-bottom','1px solid grey')
+      // $('.profileSec').css('height', '700px');
+      // $('.cardbody1').css('border-bottom', '1px solid grey')
+      // $('.cardbody2').css('border-bottom', '1px solid grey')
 
-    }else
-    {
+    } else {
       $('#heroSvgImgWeb').show();
-      $('.cardbody1').css('border-right','1px solid grey')
-      $('.cardbody2').css('border-right','1px solid grey')
+      $('.cardbody1').css('border-right', '1px solid grey')
+      $('.cardbody2').css('border-right', '1px solid grey')
     }
 
   }
-
+  email() {
+    Email.send({
+      Host: 'smtp.elasticemail.com',
+      Username: 'udith.vivekjha0151@gmail.com',
+      Password: 'C4EDC76AF01DE49850CAFF908C8EAD3369EE',
+      To: 'udith.vivekjha0151@gmail.com',
+      From: 'udith.vivekranjan9431372083@gmai.com',
+      Subject: 'test',
+      Body: 'hellloooooo'
+    }).then((message: any) => { alert(message); });
+  }
 }
