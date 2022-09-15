8a5a12a22f1dd2be51f497f13d9d2aa79f1fe178
vivek jha <vivekjha0151@gmail.com>
2022-09-15 17:55:24 +0530

    social media handels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
-import { HomepageComponent } from './homepage/homepage.component';
-import { TestComponent } from './test/test.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent,
-    TestComponent,
     PortfolioComponent
   ],
   imports: [
